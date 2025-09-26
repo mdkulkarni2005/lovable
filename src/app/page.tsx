f35563199206ad816936610135814e6d25ca0e76
mdkulkarni2005 <147185935+mdkulkarni2005@users.noreply.github.com>
2025-09-26 07:18:42 +0530

    Integrate tRPC with React Query and add AI endpoint
    Added tRPC server and client setup with React Query integration, including superjson serialization. Implemented a sample 'createAi' endpoint and connected it to the frontend with suspense and hydration. Updated dependencies to include tRPC, React Query, and related packages.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
 
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Client } from "./client";
+import { Suspense } from "react";
+
+
 const Page = async () => {
+  const queryClient = getQueryClient()
+  void queryClient.prefetchQuery(trpc.createAi.queryOptions({ text: "Hello Manas" }))
 
   return (
-    <div>
-      Hello world
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Client />
+      </Suspense>
+    </HydrationBoundary>
   )
-}
+};
+
+export default Page;
